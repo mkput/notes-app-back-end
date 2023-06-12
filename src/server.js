ee5d7e088a6dcdde7b12780d0116eaa7d0a7db23
TernakKu <ceoternakku@gmail.com>
2023-06-12 17:54:13 +0700

    Making Structur Folder

--- src/server.js
+++ src/server.js
@@ -1,18 +1,28 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesValidator = require('./validator/notes');
+const NotesServices = require('./services/inMemory/notesServices');
 
 const init = async () => {
+  const notesService = new NotesServices();
+
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
-      }
-    }
+      },
+    },
   });
 
-  server.route(routes);
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
